fd386cc79f0cd64cf1c7d4ff937561fcc95a2b49
Sean O'Gary <seanogarydev@gmail.com>
2022-02-06 18:25:12 -0500

    removed erroneous console logs

--- calculaltor_interface.js
+++ calculaltor_interface.js
@@ -1,25 +1,25 @@
-import {calculate} from "./calculator.js";
+import { calculate } from "./calculator.js";
 
-const display = document.getElementById('display');
-const btnElements = document.getElementsByClassName('btn');
-const resetBtnElement = document.getElementById('btn--reset');
-const equalsBtnElement = document.getElementById('btn--equals');
+const display = document.getElementById("display");
+const btnElements = document.getElementsByClassName("btn");
+const resetBtnElement = document.getElementById("btn--reset");
+const equalsBtnElement = document.getElementById("btn--equals");
 
-console.log("hello");
-for (let i=0; i < btnElements.length; i++) {
-    btnElements[i].addEventListener('click', function() {
-        let currentValue = display.getAttribute('value');
-        display.setAttribute('value', `${currentValue + document.getElementById(btnElements[i].id).textContent}`);
-        console.log('clicking');
-    });
-    console.log('looping');
-};
+for (let i = 0; i < btnElements.length; i++) {
+  btnElements[i].addEventListener("click", function () {
+    let currentValue = display.getAttribute("value");
+    display.setAttribute(
+      "value",
+      `${currentValue + document.getElementById(btnElements[i].id).textContent}`
+    );
+  });
+}
 
-resetBtnElement.addEventListener('click', function() {
-    display.setAttribute('value', '')
+resetBtnElement.addEventListener("click", function () {
+  display.setAttribute("value", "");
 });
 
-equalsBtnElement.addEventListener('click', function() {
-    let answer = calculate(display.getAttribute('value'));
-    display.setAttribute('value',`=${answer}`);
+equalsBtnElement.addEventListener("click", function () {
+  let answer = calculate(display.getAttribute("value"));
+  display.setAttribute("value", `=${answer}`);
 });

aac30e9b7665c247e3a5fde9319fd653f97981a2
Sean O'Gary <seanogarydev@gmail.com>
2022-02-06 18:23:40 -0500

    can now accept inputs and numbers of 'arbitrary' length!

--- calculator.js
+++ calculator.js
@@ -1,48 +1,68 @@
 export var stringToArray = (string) => {
-    let stringArray = [];
-    for (let i = 0; i < string.length; i++) {
-        console.log('function called')
-        stringArray.push(string[i]);
-    };
-    return stringArray;
+  let stringArray = [];
+  let offset = 0;
+  let currentElement;
+  let j = 0;
+  for (let i = 0; i < string.length - 1; i++) {
+    if (isNaN(string[i]) && string[i] != ".") {
+      stringArray.push(string[i]);
+      j = stringArray.length;
+    } else {
+      currentElement = stringArray[j] || "";
+      stringArray[j] = currentElement + string[i];
+    }
+  }
+  return stringArray;
 };
 
 export function calculate(expression) {
-    const orderOfOperations = ['^', '÷', 'x', '-', '+']
-    let expressionArray = stringToArray(expression);
-    console.log(expressionArray)
-    orderOfOperations.forEach(element => {
-        console.log(element);
-        let indexOfOperator = expressionArray.indexOf(element);
-        let substitute;
-        while (indexOfOperator !== -1) {
-            console.log(expressionArray);
-            indexOfOperator = expressionArray.indexOf(element);
-            switch (element) {
-                case '^':
-                    substitute = (parseInt(expressionArray[indexOfOperator - 1])**parseInt(expressionArray[indexOfOperator + 1])).toString();
-                    expressionArray.splice(indexOfOperator - 1, 3, substitute);
-                    break;
-                case 'x':
-                    substitute = (parseInt(expressionArray[indexOfOperator - 1]) * parseInt(expressionArray[indexOfOperator + 1])).toString();
-                    expressionArray.splice(indexOfOperator - 1, 3, substitute);
-                    break;
-                case '÷':
-                    substitute = (parseInt(expressionArray[indexOfOperator - 1]) / parseInt(expressionArray[indexOfOperator + 1])).toString();
-                    expressionArray.splice(indexOfOperator - 1, 3, substitute);
-                    break;
-                case '+':
-                    substitute = (parseInt(expressionArray[indexOfOperator - 1]) + parseInt(expressionArray[indexOfOperator + 1])).toString();
-                    expressionArray.splice(indexOfOperator - 1, 3, substitute);
-                    break;
-                case '-':
-                    substitute = (parseInt(expressionArray[indexOfOperator - 1]) - parseInt(expressionArray[indexOfOperator + 1])).toString();
-                    expressionArray.splice(indexOfOperator - 1, 3, substitute);
-                    break;
-            }
-            indexOfOperator = expressionArray.indexOf(element);
-        };
-    });
-    let answer = expressionArray[0];
-    return answer;
-};
+  const orderOfOperations = ["^", "÷", "x", "-", "+"];
+  let expressionArray = stringToArray(expression);
+  orderOfOperations.forEach((element) => {
+    let indexOfOperator = expressionArray.indexOf(element);
+    let substitute;
+    while (indexOfOperator !== -1) {
+      indexOfOperator = expressionArray.indexOf(element);
+      switch (element) {
+        case "^":
+          substitute = (
+            parseFloat(expressionArray[indexOfOperator - 1]) **
+            parseFloat(expressionArray[indexOfOperator + 1])
+          ).toString();
+          expressionArray.splice(indexOfOperator - 1, 3, substitute);
+          break;
+        case "x":
+          substitute = (
+            parseFloat(expressionArray[indexOfOperator - 1]) *
+            parseFloat(expressionArray[indexOfOperator + 1])
+          ).toString();
+          expressionArray.splice(indexOfOperator - 1, 3, substitute);
+          break;
+        case "÷":
+          substitute = (
+            parseFloat(expressionArray[indexOfOperator - 1]) /
+            parseFloat(expressionArray[indexOfOperator + 1])
+          ).toString();
+          expressionArray.splice(indexOfOperator - 1, 3, substitute);
+          break;
+        case "+":
+          substitute = (
+            parseFloat(expressionArray[indexOfOperator - 1]) +
+            parseFloat(expressionArray[indexOfOperator + 1])
+          ).toString();
+          expressionArray.splice(indexOfOperator - 1, 3, substitute);
+          break;
+        case "-":
+          substitute = (
+            parseFloat(expressionArray[indexOfOperator - 1]) -
+            parseFloat(expressionArray[indexOfOperator + 1])
+          ).toString();
+          expressionArray.splice(indexOfOperator - 1, 3, substitute);
+          break;
+      }
+      indexOfOperator = expressionArray.indexOf(element);
+    }
+  });
+  let answer = expressionArray[0];
+  return answer;
+}
